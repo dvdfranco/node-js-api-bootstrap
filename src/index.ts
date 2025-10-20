01148d6a9d9cd40c8cfc6ee3b0b5277aadf5d930
dvd <franco.dvd@gmail.com>
2025-10-20 20:10:13 -0300

    Simple mongodb operations with Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import { getAllCategories, updateCategory, searchCategoryByName } from './controllers/categoryController';
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello, Pockety!');
 });
+
+app.get('/api/categories', getAllCategories);
+app.put('/api/categories/:id', updateCategory);
+app.get('/api/categories/search', searchCategoryByName);
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
